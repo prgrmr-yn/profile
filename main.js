a46cf769a02704ed789a910f2633b08d56a615ff
Yatin Nagpal <nnega4428@gmail.com>
2022-11-18 17:35:03 +1100

    fixed the add event listenter

---
+++ main.js
@@ -0,0 +1,35 @@
+const username = document.getElementById('username')
+const content = document.getElementById('content')
+const send = document.getElementById('send')
+
+send.addEventListener('click', e=>{
+  alert('not sure')
+  e.preventDefault()
+  discordIt()
+  console.dir(username)
+  username.value = ''
+  content.value = ''
+  document.getElementById('sent').innerHTML = '<strong> sent</strong>'
+  setTimeout(() => {
+    document.getElementById('sent').style.display = 'none'
+  }, 2000);
+})
+
+function discordIt() {
+  fetch('https://discord.com/api/webhooks/1027741592683032677/ATWnt5El6lZBIQ1qZwzr1dN8gsH3Wcga4MlwcFbnK-KV0ztP1VTX8620MEdeewLyASUQ', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        "content": content.value,
+        "embeds": null,
+        "username": username.value,
+        "attachments": []
+      }
+    )
+  })
+  .then(response => response.json())
+  .then(response => console.log(JSON.stringify(response)))
+}
