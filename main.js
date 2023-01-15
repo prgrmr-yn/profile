a3ccd30f4fb99b0c9ed3b4183e16cff33a11f615
Yatin Nagpal <nnega4428@gmail.com>
2023-01-15 21:45:58 +1100

    added a module to hide api key , cant get dotenv to work

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
+import {webhookDiscord } from "./api.js";
+
 const username = document.getElementById('username')
 const content = document.getElementById('content')
 const send = document.getElementById('send')
+const sentMessage = document.getElementById('sent');
+let message ='';
 
 send.addEventListener('click', e=>{
   e.preventDefault()
-  discordIt()
-  username.value = ''
-  content.value = ''
-  document.getElementById('sent').innerHTML = '<strong> sent</strong>'
-  setTimeout(() => {
-    document.getElementById('sent').style.display = 'none'
-  }, 2000);
+  if (username.value === '') {
+    console.log('not valid');
+    sentMessage.innerHTML = '<strong>Must enter username</strong>'
+  }else if (content.value === '') {
+    sentMessage.innerHTML = '<strong>Must enter a message</strong>'
+  } else{
+    discordIt();
+    console.log(message);
+    username.value = ''
+    content.value = ''
+    setTimeout(() => {
+      sentMessage.style.display = 'none'
+    }, 3000);
+  }
+
 })
 
 function discordIt() {
-  fetch('https://discord.com/api/webhooks/1027741592683032677/ATWnt5El6lZBIQ1qZwzr1dN8gsH3Wcga4MlwcFbnK-KV0ztP1VTX8620MEdeewLyASUQ', {
+  fetch( webhookDiscord, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -23,11 +35,17 @@ function discordIt() {
     body: JSON.stringify({
         "content": content.value,
         "embeds": null,
-        "username": username.value,
-        "attachments": []
-      }
-    )
+        "username": username.value
+    })
   })
-  .then(response => response.json())
-  .then(response => console.log(JSON.stringify(response)))
+   .then(function(res) {
+    if (res.status.toString().startsWith('2')) {
+      message = 'Sent successfully'
+    }else {
+      message = 'Something went wront, Please try again'
+    }
+    sentMessage.innerHTML = `<strong> ${message}</strong>`
+   })
+  // .then(response => response.json())
+  // .then(response => JSON.stringify(response))
 }
