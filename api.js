84ef6f46f3ec6f476f45f59033fdd301b8d55bcb
Yatin Nagpal <nnega4428@gmail.com>
2023-02-05 17:12:45 +1100

    removed the picture and added a comment

--- api.js
+++ api.js
@@ -1,1 +1,3 @@
+// Only POST webhook no API
+
 export const webhookDiscord = 'https://discord.com/api/webhooks/1027741592683032677/ATWnt5El6lZBIQ1qZwzr1dN8gsH3Wcga4MlwcFbnK-KV0ztP1VTX8620MEdeewLyASUQ';
